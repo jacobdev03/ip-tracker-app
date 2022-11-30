ad098ae7fa588ebb58f4b7debcaf454aa53d6e73
Jacob <73615055+jacobdev03@users.noreply.github.com>
2022-11-30 17:11:45 +0100

    api calls

--- app.js
+++ app.js
@@ -14,3 +14,31 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 L.marker([51.5, -0.09], { icon: popupImg }).addTo(map).openPopup();
+
+const search = document.querySelector(".search");
+const ipval = document.querySelector("#ip-value");
+const ipaddress = document.querySelector("#ip-address");
+const locationIp = document.querySelector("#location");
+const timezone = document.querySelector("#timezone");
+const isp = document.querySelector("#isp");
+
+search.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const ip = ipval.value;
+  getIp(ip);
+});
+
+async function getIp(ip) {
+  try {
+    const resp = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_e1phcogIZHNX3W316yyScbXzNlb4x&ipAddress=${ip}`
+    );
+    const respData = await resp.json();
+    ipaddress.textContent = `${respData.ip}`;
+    locationIp.textContent = `${respData.location.city}`;
+    timezone.textContent = `${respData.location.timezone}`;
+    isp.textContent = `${respData.isp}`;
+  } catch (error) {
+    console.log(error);
+  }
+}
